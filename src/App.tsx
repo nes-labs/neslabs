bd6ca9dd2b168d01026410be0fd4770677f2b57c
georgegoldman <georgegoldmanjohn.o@gmail.com>
2025-09-05 04:46:55 +0100

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import NeslabsLanding from './NeslabsLanding'
+
+function App() {
+  const [] = useState(0)
+
+  return <NeslabsLanding />
+}
+
+export default App
